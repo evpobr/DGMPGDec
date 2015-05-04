d388d0eae760a0ba0ea10b22b15216c8056b611d
evpobr <evpobr@gmail.com>
2015-05-04 18:01:03 +0500

    Enabled precompiled headers for DGIndex

--- DGIndex/gui.cpp
+++ DGIndex/gui.cpp
@@ -20,7 +20,6 @@
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
  *
  */
-#define _WIN32_WINNT 0x0501 // Needed for WM_MOUSEWHEEL
 
 #include <windows.h>
 #include "resource.h"
